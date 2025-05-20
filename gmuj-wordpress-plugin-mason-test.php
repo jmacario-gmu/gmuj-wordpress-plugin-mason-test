19f97864d2b29bd8b22fee601d27cae00040bbdd
Jan Macario <jmacario@gmu.edu>
2025-05-20 17:05:46 -0400

    Updates to version 1.4. In preparation for new release, points to new repository location.

--- gmuj-wordpress-plugin-mason-test.php
+++ gmuj-wordpress-plugin-mason-test.php
@@ -7,9 +7,9 @@
 /**
  * Plugin Name:       Mason WordPress: Mason Test Plugin
  * Author:            Jan Macario
- * Plugin URI:        https://github.com/jmacario-gmu/gmuj-wordpress-plugin-mason-test
+ * Plugin URI:        https://github.com/mason-webmaster/gmuw-wordpress-plugin-mason-test
  * Description:       Mason WordPress plugin used for testing purposes.
- * Version:           1.3
+ * Version:           1.4
  */
 
 
@@ -21,7 +21,7 @@
 // Set up auto-updates
 	require 'plugin-update-checker/plugin-update-checker.php';
 	$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-	'https://github.com/jmacario-gmu/gmuj-wordpress-plugin-mason-test/',
+	'https://github.com/mason-webmaster/gmuw-wordpress-plugin-mason-test/',
 	__FILE__,
 	'gmuj-wordpress-plugin-mason-test'
 	);
